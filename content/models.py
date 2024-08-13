2e293875fe5fe8363988809b423c1551f26ed6e4
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-08-13 18:51:36 +0100

    Create Hashtag, Post, PostLike and Comment models

--- content/models.py
+++ content/models.py
@@ -53,3 +53,60 @@ class Follow(models.Model):
 
     class Meta:
         unique_together = (("follower", "following"),)
+
+
+class Hashtag(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Hashtag")
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
+    pub_date = models.DateTimeField(auto_now_add=True)
+    schedule_date = models.DateTimeField(null=True, blank=True, default=None)
+    image = models.ImageField(upload_to=UploadToPath(f"posts/"), null=True, blank=True)
+    text = models.TextField(max_length=500, default="")
+    hashtags = models.ManyToManyField(Hashtag, blank=True, related_name="posts")
+
+    def __str__(self):
+        return f"{self.author}'s post created at {self.pub_date}"
+
+    def get_comments(self):
+        return Comment.objects.filter(post_id=self.pk)
+
+    def get_comments_count(self):
+        return Comment.objects.filter(post=self.pk).count()
+
+    class Meta:
+        ordering = ["-pub_date"]
+
+
+class PostLike(models.Model):
+    liked = models.BooleanField(null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    liked_by = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="likes"
+    )
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.liked_by} liked {self.post}"
+
+    class Mets:
+        unique_together = (("profile", "post"),)
+        ordering = ["-liked_at"]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    commented_at = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.author} commented at {self.commented_at}: {self.text}"
+
+    class Meta:
+        ordering = ["-commented_at"]
