b9fdd0181587c5a1c1d89811ede5ad4104566b93
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-08-16 18:11:17 +0100

    Add follow/unfollow logic to Profile view class

--- content/views.py
+++ content/views.py
@@ -1,11 +1,13 @@
 from django.db.models import Count, Exists, OuterRef
-from rest_framework import mixins
+from rest_framework import mixins, status
+from rest_framework.decorators import action
 from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
     get_object_or_404,
     ListAPIView,
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from content.models import Profile, Follow, Post, PostLike, Comment
@@ -55,6 +57,8 @@ class ProfileViewSet(
             self.serializer_class = ProfileListSerializer
         if self.action == "retrieve":
             self.serializer_class = ProfileDetailSerializer
+        if self.action in ("follow", "unfollow"):
+            self.serializer_class = FollowerSerializer
         return self.serializer_class
 
     def get_queryset(self):
@@ -68,6 +72,55 @@ class ProfileViewSet(
             following_count=Count("following"),
         )
         return queryset
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="follow",
+    )
+    def follow(self, request, pk=None):
+        follower = get_object_or_404(Profile, user=self.request.user)
+        following = get_object_or_404(Profile, pk=pk)
+
+        if follower == following:
+            return Response(
+                {"detail": "You cannot follow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if Follow.objects.filter(follower=follower, following=following).exists():
+            return Response(
+                {"detail": "You are already following this user."},
+                status=status.HTTP_409_CONFLICT,
+            )
+
+        Follow.objects.create(follower=follower, following=following)
+        return Response(
+            {"detail": "You started following this user."},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="unfollow",
+    )
+    def unfollow(self, request, pk=None):
+        follower = get_object_or_404(Profile, user=self.request.user)
+        following = get_object_or_404(Profile, pk=pk)
+
+        try:
+            subscription = Follow.objects.get(follower=follower, following=following)
+            subscription.delete()
+            return Response(
+                {"detail": "You unfollowed this user."},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except Follow.DoesNotExist:
+            return Response(
+                {"detail": "You are not following this user."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
 
 class FollowersView(ListAPIView):
