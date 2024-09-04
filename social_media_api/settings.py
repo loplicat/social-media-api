0df33d12bc97c1478bd0bfdc005990bc11b984aa
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-09-04 17:52:46 +0100

    Connect PostgreSQL and create new DB

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = "social_media_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
